efb8088f0ab4e66a6b592971dc74fb6b18fc6739
Elynne Silva de Lima <164261293+Elyyihz@users.noreply.github.com>
2025-09-15 17:01:01 +0000

    Ajustando Vercel.json

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import cors from 'cors';
 import express from "express";
+import tarefasRoutes from './routes/tarefas.js';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/tarefas', tarefasRoutes);
 
